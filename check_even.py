67a64af8bc07d9caeb994ceb9e1a63edeaef4d06
kashyrskyy <56770018+kashyrskyy@users.noreply.github.com>
2021-06-30 07:58:37 -0700

    Add files via upload

---
+++ check_even.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+def is_even(n):
+    if (n %2) == 0:
+        return True
+    else:
+        return False
+#test-cases
+
+is_even(8)
+is_even(0)
+is_even(9)
+is_even(-1)
+
